3b3ecf4c8131fe5a0b552d02a0838832e2ab8a99
neuro <nanenaro@gmail.com>
2021-07-23 19:08:46 +0300

    начаата работа над предсказаниями

--- motif_predict.py
+++ motif_predict.py
@@ -11,6 +11,43 @@
 # кликаем в места предсказаний кликером, формируем структуру предсказаний.
 # бегаем мотивом по всем картинкам и в каждом месе срабатыввания собираем активации по предсказаниям,
 # складываем их в контейнер. После того, как контейнер набран, визуализируем гистограммы и в хтмл.
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from motif import *
+from logger import *
+from sensors import *
+from create_motif import *
+from init_motif import *
+from data import *
+
+def PREDICTION_EXPERIMENT(logger):
+    motif, init_coords = motif_from_json("simplest.motif")
+    desired_num_of_full_sprouts = 3
+    pics = etalons_of3()
+    predictions = init_predictions(pics[0], init_coords)
+    raw_predictions_data = gather_stat_for_predictions(pics, motif, predictions)
+    for prediction in predictions:
+
+def init_predictions(pic, init_coords):
+    pass
+
+def gather_stat_for_predictions(pics, motif, predictions):
+    raw_acivations_data = {id:[] for id in list(predictions.keys())}
+    for pic in pics:
+        dict_coords_sprouts = motif.get_sprouts_for_all_pic(pic,  desired_num_of_full_sprouts=1)
+        for coords in dict_coords_sprouts.keys():
+            for prediction_id in predictions.keys():
+                res = check_prediction(predictions[prediction_id], pic, coords[0], coords[1])
+                raw_acivations_data[prediction_id].append(res)
+    return raw_acivations_data
 
 
+def check_prediction(prediction, pic, anchorx, anchory):
+    pass
 
+if __name__ == "__main__":
+    logger = HtmlLogger("EX_PRED")
+    PREDICTION_EXPERIMENT(logger)
+    logger.close()
