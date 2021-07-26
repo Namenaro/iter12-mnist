88b9b26d55e81adb285fb0b933cb77afe6d6e373
neuro <nanenaro@gmail.com>
2021-07-26 19:10:26 +0300

    start work on main

--- prediction_stat_data.py
+++ prediction_stat_data.py
@@ -16,7 +16,7 @@ def select_random_pic(pics):
 def select_random_xoord_on_pic(pic):
     maxX = pic.shape[1]
     maxY = pic.shape[0]
-    x = random.randint(0, maxX-1)
+    x = random.randint(0, maxX - 1)
     y = random.randint(0, maxY - 1)
     return x,y
 
